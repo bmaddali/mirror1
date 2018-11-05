78306aa21d03691ebf80cb4f34b19583f9c571ec
Balaji Maddali <bmaddali@rh.com>
2018-11-05 11:37:54 -0800

    simple maven build

--- vars/buildMvn.groovy
+++ vars/buildMvn.groovy
@@ -7,21 +7,7 @@ def call(config) {
 				  //Check for Quality Gate conditions
 				  //qualityGate(config)
 
-              // publish artifact on when built from master branch.
-              if ("${BRANCH_NAME}".contains("master")){
-                    //build container and publish
-              }
 			  println "============================ BUILD MAVEN COMPLETED ===================================="
-
-              // Common job rename script
-/*              setEnv([getArtifactsVersion: "true" ]) //This is to get correct version after deploy and renaming the build correctly
-              script {
-                if (env.configVersion)
-                    currentBuild.displayName = "${env.APP_VERSION}_${env.BUILD_NUMBER}"
-                else
-                    currentBuild.displayName = "${env.APP_VERSION}"
-                }
-              } */
 
     } catch (err) {
             error("Error encountered while maven build ${err}")
