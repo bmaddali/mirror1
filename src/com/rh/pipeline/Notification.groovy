d79275aa3ed1fda3804db7cc9118121f1b743b60
Balaji Maddali <bmaddali@rh.com>
2019-01-08 20:03:43 -0800

    added Notification class

---
+++ src/com/rh/pipeline/Notification.groovy
@@ -0,0 +1,49 @@
+def notifyDeveloperBySlack(String buildStatus, String toChannel){
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'STARTED'
+  def colorCode = '#FF0000'
+  def summary = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+  try {
+    if (buildStatus == 'STARTED' || buildStatus == 'UNSTABLE') {
+    colorCode = '#FFFF00'
+    // YELLOW
+    } elseif (buildStatus == 'SUCCESSFUL') {
+    colorCode = '#00FF00'
+    // GREEN
+    } else {
+    colorCode = '#FF0000'
+    // RED
+    }
+    // Send slack notifications all messages
+    slackSend (color: colorCode, message: summary, channel: toChannel)
+  } catch (e) {
+    echo "Failed to send Slack notification : ${e}"
+    throw e
+  }
+}//end notifyDeveloperBySlack
+
+def notifyPromoterBySlack(String buildStatus, String toChannel, String toApprover){
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'STARTED'
+  def colorCode = '#FF0000'
+  def summary = "Deployment Approval for : `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+  try {
+    if (buildStatus == 'STARTED' || buildStatus == 'UNSTABLE') {
+    colorCode = '#FFFF00'
+    // YELLOW
+    } elseif (buildStatus == 'SUCCESSFUL') {
+    colorCode = '#00FF00'
+    // GREEN
+    } else {
+    colorCode = '#FF0000'
+    // RED
+    }
+    // Send slack notifications all messages
+    slackSend (color: colorCode, message: summary, channel: toChannel)
+    input (message: "Deployment Approval?", ok: 'Approve', submitter: "${toApprover}", submitterParameter: submitter)
+  } catch (e) {
+    echo "Failed to send Slack notification : ${e}"
+    throw e
+  }
+}//end notifyPromoterBySlack
