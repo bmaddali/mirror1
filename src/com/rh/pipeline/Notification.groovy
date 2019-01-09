382e55e77c70c432e3d1e2ab1eb6e2ef02de6044
Balaji Maddali <bmaddali@rh.com>
2019-01-08 20:52:09 -0800

    added Notification class

--- src/com/rh/pipeline/Notification.groovy
+++ src/com/rh/pipeline/Notification.groovy
@@ -22,23 +22,11 @@ def notifyDeveloperBySlack(String buildStatus, String toChannel){
   }
 }//end notifyDeveloperBySlack
 
-def notifyPromoterBySlack(String buildStatus, String toChannel, String toApprover){
-  // build status of null means successful
-  buildStatus = buildStatus ?: 'STARTED'
-  def colorCode = '#FF0000'
-  def summary = "Deployment Approval for : `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\input"
+def notifyPromoterBySlack(String toChannel, String toApprover){
+  def colorCode = '#FF0000' //RED
+  def summary = "Deployment Approval for : `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\\input"
 
   try {
-    if (buildStatus == 'STARTED' || buildStatus == 'UNSTABLE') {
-    colorCode = '#FFFF00'
-    // YELLOW
-    } else if (buildStatus == 'SUCCESSFUL') {
-    colorCode = '#00FF00'
-    // GREEN
-    } else {
-    colorCode = '#FF0000'
-    // RED
-    }
     // Send slack notifications all messages
     slackSend (color: colorCode, message: summary, channel: toChannel)
     input (message: "Deployment Approval?", ok: 'Approve', submitter: "${toApprover}", submitterParameter: 'submitter')
