4aeb351be1f813a042d070566819c4b6e61bacf7
Balaji Maddali <bmaddali@rh.com>
2019-02-18 10:40:20 -0800

    appbuilds

--- src/com/rh/pipeline/AppBuilds.groovy
+++ src/com/rh/pipeline/AppBuilds.groovy
@@ -2,11 +2,12 @@ package com.rh.pipeline
 
 class AppBuilds {
     
-    static def buildWithMaven(script, mvnConfigFilePath, args){
-        if (!mvnConfigFilePath) throw new IllegalArgumentException("The parameter 'mvnConfigFilePath' can not be null or empty.")
+    static def buildWithMaven(script, mvnConfigFileID, args){
+        if (!mvnConfigFileID) throw new IllegalArgumentException("The parameter 'mvnConfigFileID' can not be null or empty.")
         try {
-            withMaven(maven: 'maven', mavenSettingsConfig: mvnConfigFilePath) {
-                script.sh "mvn -o ${args}"
+            configFileProvider([configFile(fileId: mvnConfigFileID, variable: 'PACKER_OPTIONS')]) {
+                script.sh("cat ${env.PACKER_OPTIONS}")
+                script.sh "${script.tool 'maven'}/bin/mvn -s ${env.PACKER_OPTIONS} -o ${args}"
             }
         } catch(err) {
             throw(err)
