ced18219407c3b59439936d52997fb6b7da0f93a
Balaji Maddali <bmaddali@rh.com>
2019-02-22 11:36:14 -0800

    added chart version, env, and aws account

--- src/com/rh/pipeline/Deployment.groovy
+++ src/com/rh/pipeline/Deployment.groovy
@@ -4,7 +4,8 @@ import com.rh.pipeline.Notification
 
 /*
 class Deployment{
-
+    private String aws_account_number
+    private String deploy_env
     private String kubectl_contex_name
     private String deploy_release_name
     private String image_version
@@ -12,9 +13,9 @@ class Deployment{
     private String cluster_domain
     private String service_release_name
     private String helm_config_yaml
-*/  
+*/
 
-    def deployApplication(String deploy_env="nonprod", String kubectl_contex_name, String deploy_release_name,  String tiller_namespace, String helm_config_yaml, String image_version) {
+    def deployApplication(String aws_account_number, string deploy_env="nonprod", String kubectl_contex_name, String deploy_release_name,  String tiller_namespace, String helm_config_yaml, String image_version) {
         def notifyUtils = new Notification()
         if (deploy_env == "production" || deploy_env == "prodpci"){
             notifyUtils.notifyPromoterBySlack("#jenkins-ci-prod", "bmaddali,jedwards")
@@ -27,8 +28,9 @@ class Deployment{
             set -ex
 
             KUBECONFIG=/var/lib/jenkins/.kube/config-cijenkins-${kubectl_contex_name} \
-            /usr/local/bin/helm upgrade --install --debug  ${deploy_release_name} \
-            --set service.version=${image_version} --set image.tag=${image_version} \
+            /usr/local/bin/helm upgrade --install --version 1.7.0 --debug  ${deploy_release_name} \
+            --set service.version=${image_version} --set env=${deploy_env} \
+            --set-string awsAccount=${aws_account_number} --set image.tag=${image_version} \
             --tiller-namespace=${tiller_namespace} --namespace=${tiller_namespace} \
             -f ${helm_config_yaml} rhapsody-helm/rhapsody-deployment
          """
@@ -48,7 +50,7 @@ class Deployment{
             set -ex
 
             KUBECONFIG=/var/lib/jenkins/.kube/config-cijenkins-${kubelet_context_name} \
-            /usr/local/bin/helm upgrade --install --debug ${service_release_name} \
+            /usr/local/bin/helm upgrade --install --version 1.6.0 --debug ${service_release_name} \
             --tiller-namespace=${tiller_namespace} --namespace=${tiller_namespace} \
             --set clusterDomain=${cluster_domain} -f ${helm_config_yaml} rhapsody-helm/rhapsody-service
         """
