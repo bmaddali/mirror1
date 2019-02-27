bd4cd1a251c5df1f2bba31fbb616643c6735c8a5
Balaji Maddali <bmaddali@rh.com>
2019-02-26 20:34:31 -0800

    added Docker.groovy

--- src/com/rh/pipeline/CodeQuality.groovy
+++ src/com/rh/pipeline/CodeQuality.groovy
@@ -1,22 +1,29 @@
 package com.rh.pipeline
 
-/*
-class CodeQuality {
+class CodeQuality implements Serializable {
 
-    static def runSonarScan(script, args){
-        if (!args) throw new IllegalArgumentException("The parameter 'args' can not be null or empty.")
-        try {
-		    script.sh "${script.tool 'sonar'}/bin/sonar-scanner -X ${args}"
-        } catch(err) {
-            throw(err)
-        }
+    def steps
+
+    CodeQuality(steps){
+        this.steps = steps
     }
-}
-*/
 
-def sonarScan(args){
-    def scannerHome = tool 'sonar'
-    withSonarQubeEnv('RH-SonarQube') {
-        sh "${scannerHome}/bin/sonar-scanner -X ${args}"
+    def scanWithSonar(sonarSourcePath, productName, jacocoReportPath = null, appVersion) {
+        if (!sonarSourcePath) && (!productName) && (!appVersion) throw new IllegalArgumentException("Error Missing Parameters, it can not be null or empty.")
+
+        def sonar = steps.tool name: 'sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+
+        steps.withSonarQube('RH-SonarQube') {
+            steps.sh """
+                ${sonar}/bin/sonar-runner -e \\
+                -Dsonar.sources=${sonarSourcePath} \\
+                -Dsonar.projectKey=org.rh:${productName} \\
+                -Dsonar.projectName=${productName} \\
+                -Dsonar.coverage.jacoco.xmlReportPaths=${jacocoReportPath} \\
+                -Dsonar.projectVersion=${appVersion} \\
+                -Dsonar.sourceEncoding=UTF-8 \\
+    	          -Dsonar.java.binaries=.
+            """
+        }
     }
 }
---
+++ src/com/rh/pipeline/Docker.groovy
@@ -0,0 +1,26 @@
+package com.rh.pipeline
+
+class Docker {
+
+    private String dockerRegistry = 'rhartifactory-rhapsody-docker-local.jfrog.io'
+    private String registryUser = '4711f1eb-3218-4ce0-9b5c-0f9f43641469' //artifactory credentials ID
+    private String appGroup //domain
+    private String appName // miracledate
+    private String imageTag //IMAGE_VERSION
+
+    def publishAppImage(appGroup, appName, imageTag){
+
+      if (!appGroup) && (!appName) && (!imageTag)  throw new IllegalArgumentException("Error Missing Parameters, it can not be null or empty.")
+
+        try {
+            docker.withRegistry(dockerRegistry, registryUser) {
+                def appImagePath = dockerRegistry + '/' + appGroup + '/' + appName + ':' + imageTag
+                def buildImage = docker.build(appImagePath)
+                buildImage.push(appImagePath)
+            }
+        } catch (e) {
+            echo "Failed to Build Docker Image : ${e}"
+            throw e
+        }
+    }
+}
