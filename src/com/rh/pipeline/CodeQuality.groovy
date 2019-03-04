802000dc9ed906d01b25c443dd438639f8025961
Balaji Maddali <bmaddali@rh.com>
2019-03-04 01:11:03 -0800

    updated codequality and Appbuilds

--- src/com/rh/pipeline/CodeQuality.groovy
+++ src/com/rh/pipeline/CodeQuality.groovy
@@ -11,10 +11,10 @@ class CodeQuality implements Serializable {
     def scanWithSonar(String productName, String appVersion, String jacocoReportPath) {
         //if (!sonarSourcePath) && (!productName) && (!appVersion) throw new IllegalArgumentException("Error Missing Parameters, it can not be null or empty.")
 
-        def sonar = steps.tool name: 'Sonar'
+        def sonar = steps.tool name: 'sonar'
 
         steps.withSonarQubeEnv('RH-SonarQube') {
-            steps.sh "${Sonar}/bin/sonar-runner -e -Dsonar.sources=src/main/java/ -Dsonar.projectKey=org.rh:${productName} -Dsonar.projectName=${productName} -Dsonar.coverage.jacoco.xmlReportPaths=${jacocoReportPath} -Dsonar.projectVersion=${appVersion} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=."
+            steps.sh "${sonar}/bin/sonar-runner -e -Dsonar.sources=src/main/java/ -Dsonar.projectKey=org.rh:${productName} -Dsonar.projectName=${productName} -Dsonar.coverage.jacoco.xmlReportPaths=${jacocoReportPath} -Dsonar.projectVersion=${appVersion} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=."
         }
     }
 }
